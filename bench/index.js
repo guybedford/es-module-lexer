f353e10d82216da327ac0e90350350b9a26582e3
Guy Bedford <guybedford@gmail.com>
2019-08-18 03:53:10 -0400

    polish up benchmarks

--- bench/index.js
+++ bench/index.js
@@ -3,7 +3,6 @@
  */
 
 import fs from 'fs';
-import ms from 'pretty-ms';
 import c from 'kleur';
 import analyzeModuleSyntax from '../lexer.js';
 
@@ -16,56 +15,46 @@ function parse (source) {
 
 const n = 25;
 
-function test(file) {
-	console.log(c.bold.cyan(file));
+const files = fs.readdirSync('bench/samples')
+	.map(f => `bench/samples/${f}`)
+	.filter(x => x.endsWith('.js'))
+	.map(file => {
+		const source = fs.readFileSync(file);
+		return {
+			file,
+			code: source.toString(),
+			size: source.byteLength
+		};
+	});
 
-	const code = fs.readFileSync(file, 'utf-8');
+function timeRun (code) {
+	const start = process.hrtime.bigint();
+	const parsed = parse(code);
+	const end = process.hrtime.bigint(start);
+	return Math.round(Number(end - start) / 1e6);
+}
 
-	let err;
-
-	function run(code, file) {
-		try {
-			const start = process.hrtime();
-			parse(code);
-			const time = process.hrtime(start);
-
-			const duration = time[0] * 1000 | time[1] / 1e6;
-
-			return { duration };
-		} catch (e) {
-			err = e;
-			return null;
-		}
-	}
-
-	const firstRun = run(code, file);
-
-	if (firstRun === null) {
-		console.log(c.bold.red(err.toString()));
-		console.log(err.stack);
-		return;
-	}
-
-	console.log(`> Cold: ${c.bold.green(ms(firstRun.duration))}`);
-
-	// warm up
-	let i = n;
-	while (i--) run(code, file);
-
-	// take average
-	i = n;
+console.log('Cold Run, All Samples');
+{
 	let total = 0;
-	while (i--) total += run(code, file).duration;
-
-	const avg = total / n;
-	console.log(`> Warm: ${c.bold.green(ms(avg))} (average of ${n} runs)`);
+	let totalSize = 0;
+	files.forEach(({ code, size }) => {
+		totalSize += size;
+		total += timeRun(code);
+	});
+	console.log(c.bold.cyan(`bench/samples/*.js (${Math.round(totalSize / 1e3)} KiB)`));
+	console.log(`> ${c.bold.green(total + 'ms')}`);
 }
 
-let files = process.argv.slice(2);
-if (files.length === 0) {
-	files = fs.readdirSync('bench/samples').map(f => `bench/samples/${f}`).filter(x => x.endsWith('.js'));
-}
+console.log('\nWarn Runs');
+files.forEach(({ file, code, size }) => {
+	console.log(c.bold.cyan(`${file} (${Math.round(size / 1e3)} KiB)`));
 
-files.forEach(test);
+	let total = 0;
+	for (let i = 0; i < n; i++) {
+		gc();
+		total += timeRun(code);
+	}
 
-console.log('');
+	console.log(`> ${c.bold.green((total / n) + 'ms')} (average of ${n} runs)`);
+});
