b4b597b2b068ddc490ff2612f2967b6ca2f18688
Lars den Bakker <larsdenbakker@gmail.com>
2019-08-28 10:16:51 -0700

    handle allocations for very long exports (#27)

--- src/lexer.js
+++ src/lexer.js
@@ -2,7 +2,8 @@ export function parse (source) {
   if (!wasm)
     return init.then(() => parse(source));
 
-  const extraMem = (wasm.__heap_base.value || wasm.__heap_base) + source.length * 2 - wasm.memory.buffer.byteLength;
+  // need 2 bytes per code point plus analysis space so we double again
+  const extraMem = (wasm.__heap_base.value || wasm.__heap_base) + source.length * 4 +  - wasm.memory.buffer.byteLength;
   if (extraMem > 0)
     wasm.memory.grow(Math.ceil(extraMem / 65536));
 
