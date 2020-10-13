a43eeccd5a7b99455985e53fa60cb7f7a1bb53ce
Guy Bedford <guybedford@gmail.com>
2020-10-13 13:19:55 -0700

    Support big endian (#13)

--- src/lexer.js
+++ src/lexer.js
@@ -1,13 +1,18 @@
+const isLE = new Uint8Array(new Uint16Array([1]).buffer)[0] === 1;
+
 export function parse (source, name = '@') {
   if (!wasm)
     return init.then(() => parse(source));
 
+  const len = source.length + 1;
+
   // need 2 bytes per code point plus analysis space so we double again
-  const extraMem = (wasm.__heap_base.value || wasm.__heap_base) + source.length * 4 - wasm.memory.buffer.byteLength;
+  const extraMem = (wasm.__heap_base.value || wasm.__heap_base) + len * 4 - wasm.memory.buffer.byteLength;
   if (extraMem > 0)
     wasm.memory.grow(Math.ceil(extraMem / 65536));
 
-  copy(source, new Uint16Array(wasm.memory.buffer, wasm.sa(source.length), source.length + 1));
+  const addr = wasm.sa(len - 1);
+  (isLE ? copyLE : copyBE)(source, new Uint16Array(wasm.memory.buffer, addr, len));
 
   if (!wasm.parse())
     throw Object.assign(new Error(`Parse error ${name}:${source.slice(0, wasm.e()).split('\n').length}:${wasm.e() - source.lastIndexOf('\n', wasm.e() - 1)}`), { idx: wasm.e() });
@@ -19,7 +24,16 @@ export function parse (source, name = '@') {
   return [imports, exports, !!wasm.f()];
 }
 
-function copy (src, outBuf16) {
+function copyBE (src, outBuf16) {
+  const len = src.length;
+  let i = 0;
+  while (i < len) {
+    const ch = src.charCodeAt(i);
+    outBuf16[i++] = (ch & 0xff) << 8 | (ch & 0xff0) >> 8;
+  }
+}
+
+function copyLE (src, outBuf16) {
   const len = src.length;
   let i = 0;
   while (i < len)
