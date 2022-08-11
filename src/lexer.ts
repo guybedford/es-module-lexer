0c2e0941553d6d5acd0685eb81f03e1813f68602
Alexander Marks <aomarks@google.com>
2022-08-11 08:38:04 -0700

    feat: convert Wasm bridge to TypeScript (#130)

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,20 +1,21 @@
+
 export interface ImportSpecifier {
   /**
    * Module name
-   * 
+   *
    * To handle escape sequences in specifier strings, the .n field of imported specifiers will be provided where possible.
-   * 
+   *
    * For dynamic import expressions, this field will be empty if not a valid JS string.
-   * 
+   *
    * @example
    * const [imports1, exports1] = parse(String.raw`import './\u0061\u0062.js'`);
    * imports1[0].n;
    * // Returns "./ab.js"
-   * 
+   *
    * const [imports2, exports2] = parse(`import("./ab.js")`);
    * imports2[0].n;
    * // Returns "./ab.js"
-   * 
+   *
    * const [imports3, exports3] = parse(`import("./" + "ab.js")`);
    * imports3[0].n;
    * // Returns undefined
@@ -22,7 +23,7 @@ export interface ImportSpecifier {
   readonly n: string | undefined;
   /**
    * Start of module specifier
-   * 
+   *
    * @example
    * const source = `import { a } from 'asdf'`;
    * const [imports, exports] = parse(source);
@@ -37,7 +38,7 @@ export interface ImportSpecifier {
 
   /**
    * Start of import statement
-   * 
+   *
    * @example
    * const source = `import { a } from 'asdf'`;
    * const [imports, exports] = parse(source);
@@ -137,24 +138,129 @@ export interface ExportSpecifier {
   readonly le: number;
 }
 
-/**
- * Wait for init to resolve before calling `parse`.
- */
-export const init: Promise<void>;
+const isLE = new Uint8Array(new Uint16Array([1]).buffer)[0] === 1;
 
 /**
  * Outputs the list of exports and locations of import specifiers,
  * including dynamic import and import meta handling.
- * 
+ *
  * @param source Source code to parser
  * @param name Optional sourcename
  * @returns Tuple contaning imports list and exports list.
  */
-export function parse(
-  source: string,
-  name?: string
-): readonly [
+export function parse (source: string, name = '@'): readonly [
   imports: ReadonlyArray<ImportSpecifier>,
   exports: ReadonlyArray<ExportSpecifier>,
   facade: boolean
-];
+] {
+  if (!wasm)
+    // actually returns a promise if init hasn't resolved (not type safe).
+    // casting to avoid a breaking type change.
+    return init.then(() => parse(source)) as unknown as ReturnType<typeof parse>;
+
+  const len = source.length + 1;
+
+  // need 2 bytes per code point plus analysis space so we double again
+  const extraMem = (wasm.__heap_base.value || wasm.__heap_base) as number + len * 4 - wasm.memory.buffer.byteLength;
+  if (extraMem > 0)
+    wasm.memory.grow(Math.ceil(extraMem / 65536));
+
+  const addr = wasm.sa(len - 1);
+  (isLE ? copyLE : copyBE)(source, new Uint16Array(wasm.memory.buffer, addr, len));
+
+  if (!wasm.parse())
+    throw Object.assign(new Error(`Parse error ${name}:${source.slice(0, wasm.e()).split('\n').length}:${wasm.e() - source.lastIndexOf('\n', wasm.e() - 1)}`), { idx: wasm.e() });
+
+  const imports: ImportSpecifier[] = [], exports: ExportSpecifier[] = [];
+  while (wasm.ri()) {
+    const s = wasm.is(), e = wasm.ie(), a = wasm.ai(), d = wasm.id(), ss = wasm.ss(), se = wasm.se();
+    let n;
+    if (wasm.ip())
+      n = decode(source.slice(d === -1 ? s - 1 : s, d === -1 ? e + 1 : e));
+    imports.push({ n, s, e, ss, se, d, a });
+  }
+  while (wasm.re()) {
+    const s = wasm.es(), e = wasm.ee(), ls = wasm.els(), le = wasm.ele();
+    const n = source.slice(s, e), ch = n[0];
+    const ln = ls < 0 ? undefined : source.slice(ls, le), lch = ln ? ln[0] : '';
+    exports.push({
+      s, e, ls, le,
+      n: (ch === '"' || ch === "'") ? decode(n) : n,
+      ln: (lch === '"' || lch === "'") ? decode(ln) : ln,
+    });
+  }
+
+  function decode (str: string | undefined) {
+    try {
+      return (0, eval)(str as string) // eval(undefined) -> undefined
+    }
+    catch (e) {}
+  }
+
+  return [imports, exports, !!wasm.f()];
+}
+
+function copyBE (src: string, outBuf16: Uint16Array) {
+  const len = src.length;
+  let i = 0;
+  while (i < len) {
+    const ch = src.charCodeAt(i);
+    outBuf16[i++] = (ch & 0xff) << 8 | ch >>> 8;
+  }
+}
+
+function copyLE (src: string, outBuf16: Uint16Array) {
+  const len = src.length;
+  let i = 0;
+  while (i < len)
+    outBuf16[i] = src.charCodeAt(i++);
+}
+
+let wasm: {
+  __heap_base: {value: number} | number & {value: undefined};
+  memory: WebAssembly.Memory;
+  parse(): boolean;
+  /** getAssertIndex */
+  ai(): number;
+  /** getErr */
+  e(): number;
+  /** getExportEnd */
+  ee(): number;
+  /** getExportLocalEnd */
+  ele(): number;
+  /** getExportLocalStart */
+  els(): number;
+  /** getExportStart */
+  es(): number;
+  /** facade */
+  f(): boolean;
+  /** getImportDynamic */
+  id(): number;
+  /** getImportEnd */
+  ie(): number;
+  /** getImportSafeString */
+  ip(): number;
+  /** getImportStart */
+  is(): number;
+  /** readExport */
+  re(): boolean;
+  /** readImport */
+  ri(): boolean;
+  /** allocateSource */
+  sa(utf16Len: number): number;
+  /** getImportStatementEnd */
+  se(): number;
+  /** getImportStatementStart */
+  ss(): number;
+};
+
+
+/**
+ * Wait for init to resolve before calling `parse`.
+ */
+export const init = WebAssembly.compile(
+  (binary => typeof Buffer !== 'undefined' ? Buffer.from(binary, 'base64') : Uint8Array.from(atob(binary), x => x.charCodeAt(0)))
+  ('WASM_BINARY')
+)
+.then(WebAssembly.instantiate)
+.then(({ exports }) => { wasm = exports as typeof wasm; });
